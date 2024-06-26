3eac8d453bc9a29aa2f440e46c76fbecb35cadba
Priyanka Prabashwara <63540465+prabashwara65@users.noreply.github.com>
2024-06-26 17:00:14 +0530

    Update CartContext.js

--- Components/CartContext.js
+++ Components/CartContext.js
@@ -1,7 +1,7 @@
 
 import { createContext, useEffect, useState } from "react";
 
-export const CartContext = createContext({});
+
 
 export function CartContextProvider({ children }) {
   const ls = typeof window !== "undefined" ? window.localStorage : null;
